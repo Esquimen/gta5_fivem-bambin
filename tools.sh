bbadafc13d84f2f674241c8a330eef419c26ffdb
Mathieu Mari <contact@mathieumari.com>
2023-04-16 17:57:30 +0200

    upd

--- tools.sh
+++ tools.sh
@@ -1,8 +1,8 @@
 # @author: Mathieu MARI <contact@mathieumari.com>
 # @date:   04-12-2021 01:49:51
 # @lastModifiedBy:   Mathieu MARI <contact@mathieumari.com>
-# @lastModifiedTime: 2023-04-16 14:53:28
+# @lastModifiedTime: 2023-04-16 17:44:33
 
 #!/bin/bash
 
-clear && sh 1_app/gta5_fivem-maestra/0_deploy/tools.sh
+clear && sh 1_app_fivem/gta5_fivem-maestra/0_deploy/tools.sh
