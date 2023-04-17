add12b01e2e25730402424cc8ac56c807aebfcff
Mathieu Mari <contact@mathieumari.com>
2023-04-17 20:51:55 +0200

    opti client object notification

--- 1_resources_fivem/copCore/client/objects/notifications.lua
+++ 1_resources_fivem/copCore/client/objects/notifications.lua
@@ -2,14 +2,27 @@
 --@author: Mathieu MARI <contact@mathieumari.com>
 --@date:   16-04-2023 18:27:19
 --@lastModifiedBy:   Mathieu MARI <contact@mathieumari.com>
---@lastModifiedTime: 2023-04-17 17:01:23
+--@lastModifiedTime: 2023-04-17 20:51:34
 --]]
 
 copCore.objects.notifications = {}
 
 -- ----------------------------------------------------------------------------------- Fx qui permet de faire une notification dans le jeu
 	function copCore.objects.notifications.addNew(text)
-		SetNotificationTextEntry("STRING")
-		AddTextComponentSubstringPlayerName(text)
-		DrawNotification(false, false)
+		--print(text)
+
+		Citizen.CreateThread(function()
+			-- On attend que l'écran de chargement soit complètement chargé pour afficher la notification
+			while not IsScreenFadedIn() do
+				Citizen.Wait(0)
+			end
+			
+			-- Affichage de la notification
+			SetNotificationTextEntry("STRING")
+			AddTextComponentString(text)
+			DrawNotification(false, false)
+			
+		end)
+
 	end
+	--copCore.objects.notifications.addNew("~r~Chargement de copCore.")
--- 1_resources_fivem/copCore/fxmanifest.lua
+++ 1_resources_fivem/copCore/fxmanifest.lua
@@ -2,7 +2,7 @@
 --@author: Mathieu MARI <contact@mathieumari.com>
 --@date:   16-04-2023 22:36:18
 --@lastModifiedBy:   Mathieu MARI <contact@mathieumari.com>
---@lastModifiedTime: 2023-04-17 17:00:58
+--@lastModifiedTime: 2023-04-17 20:44:02
 --]]
 
 fx_version 'cerulean'
@@ -29,6 +29,7 @@ version '1.0.0'
 	client_scripts{
 		"client/core.lua",
 		"client/events/**/*.lua",
+		"client/objects/*.lua"
 	}
 
 	client_exports {
